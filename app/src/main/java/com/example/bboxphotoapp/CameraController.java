b26958f6a81af2290a7886335a4d67b51cbe4ab1
mdhach <mdhach@protonmail.com>
2022-04-28 15:09:26 -0400

    implemented image capture/saving

--- app/src/main/java/com/example/bboxphotoapp/CameraController.java
+++ app/src/main/java/com/example/bboxphotoapp/CameraController.java
@@ -1,50 +1,56 @@
 package com.example.bboxphotoapp;
 
-import android.Manifest;
 import android.app.Activity;
+import android.content.ContentValues;
 import android.content.Context;
-import android.content.ContextWrapper;
-import android.content.pm.PackageManager;
-import android.view.LayoutInflater;
-import android.view.View;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
-import androidx.camera.core.Camera;
 import androidx.camera.core.CameraSelector;
+import androidx.camera.core.ImageCapture;
+import androidx.camera.core.ImageCaptureException;
 import androidx.camera.core.Preview;
 import androidx.camera.lifecycle.ProcessCameraProvider;
 import androidx.camera.view.PreviewView;
-import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.lifecycle.LifecycleOwner;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.common.util.concurrent.ListenableFuture;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
 
 public class CameraController {
 
-    private ListenableFuture<ProcessCameraProvider> cameraProviderFuture;
     private Context context;
     private PreviewView previewView;
     private Activity activity;
-    private AppTools appTools = new AppTools();
 
-    private static final String[] CAMERA_PERMISSION = new String[]{Manifest.permission.CAMERA};
-    private static final int CAMERA_REQUEST_CODE = 10;
+    // camera variables
+    private ListenableFuture<ProcessCameraProvider> cameraProviderFuture;
+    private ImageCapture imageCapture;
 
     public CameraController(Context context) {
         this.context = context;
-        this.activity = appTools.getActivity(this.context);
+        this.activity = Utils.getActivity(this.context);
         this.previewView = this.activity.findViewById(R.id.previewView);
 
-        if(!hasPermission()) { requestPermission(); }
+        // access camera and start-up camera
+        startCamera();
     }
 
-    public void startCamera() {
+    private void startCamera() {
+        // create camera singleton
         cameraProviderFuture = ProcessCameraProvider.getInstance(this.context);
 
+        // add camera listener
         cameraProviderFuture.addListener(() -> {
             try {
+                // check for camera instance and bind it to a preview
                 ProcessCameraProvider cameraProvider = cameraProviderFuture.get();
                 bindPreview(cameraProvider);
             } catch(Exception e) {
@@ -57,7 +63,7 @@ public class CameraController {
         // builds an immutable camera preview
         Preview preview = new Preview.Builder().build();
 
-        // select camera
+        // select camera lens
         CameraSelector cameraSelector = new CameraSelector
                 .Builder()
                 .requireLensFacing(CameraSelector.LENS_FACING_BACK)
@@ -66,17 +72,59 @@ public class CameraController {
         // provides a surface for the camera preview
         preview.setSurfaceProvider(previewView.getSurfaceProvider());
 
-        // starts data capture
-        Camera camera = cameraProvider.bindToLifecycle((LifecycleOwner) this.activity, cameraSelector, preview);
+        // instantiate image capture object
+        imageCapture = new ImageCapture.Builder()
+                .setTargetRotation(this.previewView.getDisplay().getRotation())
+                .build();
+
+        // bind image capture object to current camera instance.
+        // starts data capture; opens camera preview for end-user
+        cameraProvider.bindToLifecycle(
+                (LifecycleOwner) this.activity,
+                cameraSelector,
+                imageCapture,
+                preview);
     }
 
-    private boolean hasPermission() {
-        return ContextCompat.checkSelfPermission(this.context, Manifest.permission.CAMERA) ==
-                PackageManager.PERMISSION_GRANTED;
+    public void takePhoto() {
+        // take picture; toast messages for confirmation
+        imageCapture.takePicture(getOutputOptions(),
+                ContextCompat.getMainExecutor(this.activity),
+                new ImageCapture.OnImageSavedCallback() {
+            @Override
+            public void onImageSaved(ImageCapture.OutputFileResults outputFileResults) {
+                Toast.makeText(context,
+                        "Image saved",
+                        Toast.LENGTH_SHORT).show();
+            }
+            @Override
+            public void onError(ImageCaptureException error) {
+                error.printStackTrace();
+                Toast.makeText(context,
+                        "Error: Picture could not be saved/taken.",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
-    private void requestPermission() {
-        ActivityCompat.requestPermissions(this.activity, CAMERA_PERMISSION, CAMERA_REQUEST_CODE);
-    }
+    private ImageCapture.OutputFileOptions getOutputOptions() {
+        // output image name; current date and time
+        String outputName = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss",
+                Locale.getDefault()).format(new Date());
 
+        // MediaStore values
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(MediaStore.MediaColumns.DISPLAY_NAME, outputName);
+        contentValues.put(MediaStore.MediaColumns.MIME_TYPE, "image/jpeg");
+        contentValues.put(MediaStore.MediaColumns.RELATIVE_PATH, Environment.DIRECTORY_DCIM);
+
+        // set image capture output options
+        ImageCapture.OutputFileOptions outputFileOptions =
+                new ImageCapture.OutputFileOptions.Builder(
+                        this.activity.getContentResolver(),
+                        MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                        contentValues).build();
+
+        return outputFileOptions;
+    }
 }
